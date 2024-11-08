22b92d3d9e63caf6f717bd5baa016608e05d4430
tiffany <tiff0207@bu.edu>
2024-11-07 22:33:36 -0500

    remove direct path

--- src/MemeCat.jsx
+++ src/MemeCat.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Lottie from 'lottie-react'; // Import Lottie as the default export
-import catAnimation from '/home/tiffanybao/tiffany-react/personal-portfolio/src/assets/Animation - 1729199987120.json'; // Ensure the path is correct
-import dogAnimation from '/home/tiffanybao/tiffany-react/personal-portfolio/src/assets/LET_ME_DO_IT_FOR_YOU.json'
+import catAnimation from './assets/Animation - 1729199987120.json'; // Updated to use relative path
+import dogAnimation from './assets/LET_ME_DO_IT_FOR_YOU.json'; // Updated to use relative path
 
 const MemeCat = () => (
   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
@@ -9,20 +9,10 @@ const MemeCat = () => (
   </div>
 );
 
-
-// const LongNoseDog = () => (
-//   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
-//     <Lottie animationData={dogAnimation} style={{ width: 300, height: 300 }} loop />
-//     {/* <p style={{ fontSize: '1.5rem', marginTop: '5px' }}>Let me do it for you...</p> */}
-//   </div>
-// );
-
 const LongNoseDog = () => (
   <div style={{ float: 'right', margin: '10px', width: 100, height: 100 }}>
     <Lottie animationData={dogAnimation} style={{ width: '100%', height: '100%' }} loop />
   </div>
 );
 
-
 export { MemeCat, LongNoseDog };
-// export { MemeCat };
